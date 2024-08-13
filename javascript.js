630c5c416e03299ae0069fdf59a9c71884b66548
Midhin Lal <midhincr7@gmail.com>
2024-08-13 09:17:17 +0530

    Add buttons and colors to page
    Added more options to edit sketchpad and made added page colors

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 let container = document.querySelector(".container");
+for(let i=0;i<16*16; i++){
+    let newDiv = document.createElement("div");
+    newDiv.className = "boxes";
+    container.appendChild(newDiv);
+}
+
 let button = document.querySelector("button");
 button.addEventListener("click", function(){
     container.innerHTML = "";
@@ -11,42 +17,40 @@ button.addEventListener("click", function(){
         newDiv.style.height = `${boxSize}px`;
         newDiv.style.width = `${boxSize}px`;
         container.appendChild(newDiv);
+    }
+});
 
-        container.addEventListener("mousemove", function(e){
+let btnOptions = document.querySelector(".btn-options");
+btnOptions.addEventListener("click", function(e){
+    if(e.target.className === "rgbbtn"){
+        container.addEventListener("mouseover", function(e){
             if(e.target.className === "boxes"){
                 let rgb1 = (Math.random() *256);
                 let rgb2 = (Math.random() *256);
                 let rgb3 = (Math.random() *256);
                 e.target.style.backgroundColor = `rgb(${rgb1},${rgb2},${rgb3})`;
             }
-        }); 
-    }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    for(let i=0;i<16*16; i++){
-        let newDiv = document.createElement("div");
-        newDiv.className = "boxes";
-        container.appendChild(newDiv);
+        });
     }
 
-    container.addEventListener("mousemove", function(e){
-        if(e.target.className === "boxes"){
-            let rgb1 = (Math.random() *256);
-            let rgb2 = (Math.random() *256);
-            let rgb3 = (Math.random() *256);
-            e.target.style.backgroundColor = `rgb(${rgb1},${rgb2},${rgb3})`;
-        }
-    });
-});
+    if(e.target.className === "blackbtn"){
+        container.addEventListener("mouseover", function(e){
+            if(e.target.className === "boxes"){
+                e.target.style.backgroundColor = 'black';
+            }
+        });
+    }
 
+    if(e.target.className === "eraserbtn"){
+        container.addEventListener("mouseover", function(e){
+            if(e.target.className === "boxes"){
+                e.target.style.backgroundColor = 'white';
+            }
+        });
+    }
 
-
-let blackbtn = document.querySelector(".blackbtn");
-blackbtn.addEventListener("click", function(){
-    container.addEventListener("mouseover", function(){
-        if(e.target.className === "boxes"){
-            e.target.style.backgroundColor = 'black';
-        }
-    });
+    if(e.target.className === "clearbtn"){
+        let boxes = document.querySelectorAll(".boxes");
+        boxes.forEach(box => box.style.backgroundColor = "white");
+    }
 });
