8e37d118d72b768d87ebf7901ffdeca4a2f01eb4
midhin11 <midhincr7@gmail.com>
2025-11-01 20:05:05 +0530

    Add rgb, black and clar options
    Added colors in etch a sketch to be added in rgb or black and also to
    clear out the sketchpad.

--- js.js
+++ js.js
@@ -1,11 +1,29 @@
 let num = 16;
 let grid = document.querySelector(".grid");
-grid(num);
+gridGen(num);
+let color = "rgb";
+let black = document.querySelector(".black");
+black.addEventListener("click", () => {
+    color = 'black';
+})
+let rgb = document.querySelector(".rgb");
+rgb.addEventListener("click", () => {
+    color = 'rgb';
+})
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.style.backgroundColor = ``);
+})
 
 let gridSize = document.querySelector(".gridsize");
 gridSize.addEventListener("click", () => {
-    num = prompt("Select the grid size.", 16);
-    grid(num);
+    grid.innerHTML = "";
+    num = prompt("Select the grid size. (<100)", 16);
+    if (num>100) {
+        num = 16;
+    }
+    gridGen(num);
 })
 
 function colorGenerator() {
@@ -15,16 +33,30 @@ function colorGenerator() {
     return [r, g, b];
 }
 
-function grid(num) {
+function gridGen(num) {
     for(let i=0; i<num*num; i++){
         let newBox = document.createElement("div");
         newBox.classList.add("box");
         grid.appendChild(newBox);
+        newBox.style.height = `${500/num}px`;
+        newBox.style.width = `${500/num}px`;
 
         newBox.addEventListener("mouseenter", (e) => {
-            let rgb = colorGenerator();
-            e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            if(newBox.style.backgroundColor === ""){
+                if(color === 'black'){
+                    e.target.style.backgroundColor = `black`;
+                } else{
+                    let colorGen = colorGenerator();
+                    e.target.style.backgroundColor = `rgb(${colorGen[0]}, ${colorGen[1]}, ${colorGen[2]})`;
+                }
+            }
+            else {
+                return;
+            }
         })
     }
 }
+
+console.log(`${grid.clientHeight/num}px`    )
+
 // console.log(Math.floor(Math.random()*256)+1);
